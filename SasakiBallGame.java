0f5f03ea1eb3db4d2fdbea01aa8158fe8232a727
arukreid <sasaki3y98521j@gmail.com>
2022-03-31 11:34:13 +0900

    SasakiBallGame 修正

--- SasakiBallGame.java
+++ SasakiBallGame.java
@@ -5,6 +5,7 @@ public class SasakiBallGame{
 		int[] balls = new int[100];
 		int[] ballA = new int[round];
 		int[] ballB = new int[round];
+		String winner;
 		int winBallA = 0;
 		int winBallB = 0;
 		//ボール番号を割り振る
@@ -19,22 +20,21 @@ public class SasakiBallGame{
 			balls[i] = temp;
 		}
 		//ゲーム開始
-		int num =0;
 		for(int i=0;i<round;i++){
 			//交互にボールを取る
-			ballA[i] = balls[num];
-			num++;
-			ballB[i] = balls[num];
-			num++;
-			//AとBを比較して、それぞれの勝利回数カウント
+			ballA[i] = balls[i];//先頭から順番に取っていく
+			ballB[i] = balls[balls.length -1 -i];//後ろから順番に取っていく
+			//AとBを比較して、それぞれの勝利回数カZZZウント
 			if(ballA[i] > ballB[i]){
+				winner = "A";
 				winBallA++;
 			}else{
+				winner = "B";
 				winBallB++;
 			}
 			//各対戦の結果表示
 			System.out.printf("%d回戦%n",i+1);
-			System.out.printf("A:%3d,B:%3d...%sの勝ち%n",ballA[i],ballB[i],ballA[i]>ballB[i]?"A":"B");
+			System.out.printf("A:%3d,B:%3d...%sの勝ち%n",ballA[i],ballB[i],winner);
 		}
 		//最終結果表示
 		System.out.printf("%d対%dで%sの勝ち",winBallA,winBallB,winBallA>winBallB?"A":"B");
